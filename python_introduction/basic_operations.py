2408fe43ffc6d0eb98583a211d5c423a3a5f696f
John <jmnjogo@gmail.com>
2024-09-15 19:38:39 +0300

    task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+
+print(f"Addition of {number1} and {number2} is {addition}")
+
+difference = number1 - number2
+
+print(f"Subtraction of {number1} and {number2} is {difference}")
+
+product = number1 * number2
+
+print(f"Multiplication of {number1} and {number2} is {product}")
