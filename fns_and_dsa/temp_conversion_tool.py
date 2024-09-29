cc689db2b5a4f508c7450148b8771414f7fc4386
john <jmnjogo@gmail.com>
2024-09-29 18:15:50 +0300

    correction1

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -22,7 +22,7 @@ def main():
             print("Invalid unit. Please enter 'C' OR 'F'. ")
 
     except ValueError:
-        print("Invalid temperature.Please enter a numeric value.")
+        print("Invalid temperature. Please enter a numeric value.")
 
 if __name__=="__main__":
     main()
